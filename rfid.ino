d9fc4820238242eda729f526ec8d28b4c296bdac
gieskiverz <gie.skiverz@gmail.com>
2023-08-19 21:53:06 +0700

    change

--- rfid.ino
+++ rfid.ino
@@ -1,47 +1,31 @@
-#include <WiFi.h>
+#include <SPI.h>
+#include <MFRC522.h>
 
-const char* ssid = "ajajwjwjaj";       // Your Wi-Fi SSID
-const char* password = "egi123456"; // Your Wi-Fi password
+#define RST_PIN     5    // Define the RST_PIN (reset) for RC522 module
+#define SS_PIN      4    // Define the SS_PIN (SDA) for RC522 module
+
+MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance
 
 void setup() {
-  Serial.begin(9600);
-  delay(1000);
-
-  // Connect to Wi-Fi
-  Serial.println("Connecting to Wi-Fi...");
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
-  }
-
-  // Print initial connection status
-  if (WiFi.status() == WL_CONNECTED) {
-    Serial.println("Connected to Wi-Fi");
-  } else {
-    Serial.println("Not connected to Wi-Fi");
-  }
+  Serial.begin(115200);  // Initialize serial communication
+  
+  SPI.begin();           // Initialize SPI bus
+  mfrc522.PCD_Init();    // Initialize MFRC522 RFID module
+  
+  Serial.println("RFID Reader is ready!");
 }
 
 void loop() {
-  // Check Wi-Fi connection status
-  if (WiFi.status() != WL_CONNECTED) {
-    Serial.println("Wi-Fi disconnected. Reconnecting...");
-    WiFi.begin(ssid, password);
-
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(1000);
-      Serial.println("Reconnecting to WiFi...");
+  // Look for new RFID cards
+  if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
+    Serial.print("UID tag :");
+    String content = "";
+    for (byte i = 0; i < mfrc522.uid.size; i++) {
+      content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+      content.concat(String(mfrc522.uid.uidByte[i], HEX));
     }
-
-    // Print reconnection status
-    if (WiFi.status() == WL_CONNECTED) {
-      Serial.println("Reconnected to Wi-Fi");
-    } else {
-      Serial.println("Still not connected to Wi-Fi");
-    }
+    content.toUpperCase();
+    Serial.println(content);
+    delay(1000); // Delay to avoid reading the same card multiple times
   }
-
-  // Your main code here
 }
